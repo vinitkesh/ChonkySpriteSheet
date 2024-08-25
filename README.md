382787117082e9e20224f2f266bf62ab4aece10d
Vinit Keshri <vinitkeshri2020@gmail.com>
2024-08-26 01:45:25 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ChonkySpriteSheet
 Simple Sprite animations of a Chonky Cat to demonstrate the popular sprite sheet animations in game dev.
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+// on clicking .Charecter, move it 320px down(if down not possible, move it 640px up)
+
+document.querySelector('.Charecter').addEventListener('click', function () {
+    let top = parseInt(this.style.top) || 0;
+    if (top < 320) {
+        this.style.top = (top + 320) + 'px';
+    } else {
+        this.style.top = (top - 640) + 'px';
+    }
+} );
